fc1a97d7a679e5e2b817b2a8106c13b4e7f61ce9
Dr.Ra <theara2048@gmail.com>
2022-03-16 16:48:07 +0700

    update some login from office pc

---
+++ test.js
@@ -0,0 +1,11 @@
+
+var test = {};
+
+for(let i=0 ; i < 10; i++) {
+
+  test = Object.assign({},{'test':i});
+
+}
+
+
+console.log(test);
--- weread.js
+++ weread.js
@@ -1,44 +1,21 @@
-var https = require('https');
-
 const prompt = require('prompt');
 var fs = require('fs');
 var request = require('request');
 var _cliProgress = require('cli-progress');
-const console = require('console');
-const Queue = require('async-await-queue');
 
-/**
- * No more than 10 parallel, spaced at least 100ms apart
- * These are typical fair-use limitations of public APIs
- **/
- const queue = new Queue(10, 100);
+
  var p = [];
+ var b =[];
 
-
-var download_https = function(url) {
-    https.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);
-    });
-  });
-}
 const progressBar = new _cliProgress.MultiBar({  
     clearOnComplete: false,
     hideCursor: true},
     _cliProgress.Presets.shades_classic);
 
-var download = function(url, filename, callback) {
+var download = function(url, filename, bar_index, callback) {
   url = 'https://weread-oss.weread.asia/' + url;
-  console.log(url);
   var file = fs.createWriteStream(filename);
-  // http.get(url, function(response) {
-  //   response.pipe(file);
-  //   file.on('finish', function() {
-  //     file.close(cb);
-  //   });
-  // });
-  // const progressBar = new _cliProgress.SingleBar({format:'{bar} {percentage}% | ETA: {eta}s'},_cliProgress.Presets.shades_classic);
+
   let receivedBytes = 0;
     request.get(encodeURI(url))
       .on('response', (response) => {
@@ -46,28 +23,28 @@ var download = function(url, filename, callback) {
               return callback('Response status was ' + response.statusCode);
           }
 
-          const totalBytes = response.headers['content-length'];
-          progressBar.create(totalBytes, 0);
+      const totalBytes = response.headers['content-length'];
+        b[bar_index] =  progressBar.create(totalBytes, 0);
       })
       .on('data', (chunk) => {
           receivedBytes += chunk.length;
-          progressBar.update(receivedBytes,{filename: filename});
+          b[bar_index].update(receivedBytes,{filename: 'test.txt'});
       })
       .pipe(file)
       .on('error', (err) => {
           fs.unlink(filename);
-          progressBar.stop();
+          b[bar_index].stop();
           return callback(err.message);
       });
       file.on('finish', () => {
-          progressBar.stop();
-          console.log("File Downloaded!")
+          //progressBar.stop();
+          // console.log("File Downloaded!")
           file.close(callback);
       });
   
       file.on('error', (err) => {
           fs.unlink(filename); 
-          progressBar.stop();
+          b[bar_index].progressBar.stop();
           return callback(err.message);
       });
 
@@ -105,22 +82,12 @@ let  fetchCourses =  () => {
           if(!error && response.statusCode === 200) {
             let data = JSON.parse(body);
             for(let i=0; i<data.list.length ; i++){
-              // /* Generate a queue ID */
-              // const me = Symbol();
-              // /* 0 is the priority, -1 is higher priority than 0 */
-              // p.push(queue.wait(me, 0)
-              // .then(() => download(data.list[i].audioUrl,data.list[i].title+'.mp3'))
-              // .catch((e) => console.error(e))
-              // /* don't forget this or you will end up freezing */
-              // .finally(() => queue.end(me)));
-      
-              // download(data.list[i].audioUrl,data.list[i].title.replace(/\s/g, "")+'.mp3');
-              console.log(i);
-              download(data.list[i].audioUrl,data.list[i].title.replace(/\s/g, "")+'.mp3');
+
+              b.push(i);
+              download(data.list[i].audioUrl,data.list[i].title.replace(/\s/g, "")+'.mp3' ,i);
               // download(data.list[i].videoUrl,data.list[i].title+'.m4v');
            
             }
-            // await Promise.allSettled(p);
           }
           });
       });
