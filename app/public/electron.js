62cf1767b812e7c103c72586c514650397387378
Dr.Ra <theara2048@gmail.com>
2022-05-25 16:00:40 +0700

    lastest from office pc

--- app/public/electron.js
+++ app/public/electron.js
@@ -2,7 +2,7 @@ const path = require('path');
 const axios = require('axios');
 
 const { app, BrowserWindow ,ipcMain } = require('electron');
-const isDev = require('electron-is-dev');
+// const isDev = require('electron-is-dev');
 
 function createWindow() {
   // Create the browser window.
@@ -21,14 +21,12 @@ function createWindow() {
   // and load the index.html of the app.
   // win.loadFile("index.html");The
   win.loadURL(
-    isDev
-    ? 'http://localhost:3000'
-    : `file://${path.join(__dirname, '../build/index.html')}`
+    `file://${path.join(__dirname, '../build/index.html')}`
     );
     // Open the DevTools.
-    if (isDev) {
-      win.webContents.openDevTools({ mode: 'detach' });
-    }
+    // if (isDev) {
+    //   win.webContents.openDevTools({ mode: 'detach' });
+    // }
   }
   
 const  handleTest = async () =>{
--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import './App.css';
 import Topbar from './components/Topbar';
 import Body from './components/Body';
